62c5dbfe8f8f1af24e5c132e9ffa168589a23f2e
azer7777 <azertyuio123a77@gmail.com>
2024-05-28 00:12:59 +0200

    start spring boot project

---
+++ src/main/java/com/example/rental_portal/RentalPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.rental_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalPortalApplication.class, args);
+	}
+
+}
